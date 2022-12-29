b03974b9fc7f61d3d12f3e8176ff502d0fd1003b
S A Saharukh <sasaharukh@gmail.com>
2022-12-29 23:31:46 +0600

    chnage logo

--- lib/details.dart
+++ lib/details.dart
@@ -19,25 +19,25 @@ class DetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.indigo,
+        backgroundColor: Colors.white,
         centerTitle: true,
         title: Text(
           'পোকা মাকড় বিশারদ',
           style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.w500,
+            color: Colors.green,
+            fontWeight: FontWeight.bold,
             fontSize: 23,
           ),
         ),
       ),
       body: Container(
-        color: Color.fromRGBO(68, 190, 255, 0.8),
+        // color: Color.fromRGBO(68, 190, 255, 0.8),
         padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
         child: Container(
           alignment: Alignment.center,
           padding: EdgeInsets.all(30),
           decoration: BoxDecoration(
-            color: Colors.indigo,
+            color: Color(0xffD8DBD2),
             borderRadius: BorderRadius.circular(30),
           ),
           child: ListView(
@@ -51,7 +51,7 @@ class DetailsPage extends StatelessWidget {
               Text((confidence * 100).toStringAsFixed(2).toString() + "%",
                   style: TextStyle(
                       fontSize: 10,
-                      color: Colors.white,
+                      color: Colors.black,
                       fontWeight: FontWeight.bold)),
               //stylish text
               Divider(),
@@ -59,50 +59,51 @@ class DetailsPage extends StatelessWidget {
               Text("রোগের নাম",
                   style: TextStyle(
                       fontSize: 25,
-                      color: Colors.white,
+                      color: Colors.black,
                       fontWeight: FontWeight.bold)),
               //replace all number with bangla number
               Text(
-                  dataModel.name
-                      .replaceAll(RegExp(r"[0-9]"), "")
-                      .replaceAll("-", ""),
-                  style: TextStyle(fontSize: 20, color: Colors.white)),
+                dataModel.name
+                    .replaceAll(RegExp(r"[0-9]"), "")
+                    .replaceAll("-", ""),
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
               Divider(),
               Divider(),
               Text("উপসর্গ",
                   style: TextStyle(
                       fontSize: 25,
-                      color: Colors.white,
+                      color: Colors.black,
                       fontWeight: FontWeight.bold)),
               Text(dataModel.simtom,
-                  style: TextStyle(fontSize: 20, color: Colors.white)),
+                  style: TextStyle(fontSize: 20, color: Colors.black)),
               Divider(),
               Divider(),
               Text("জৈব নিয়ন্ত্রণ",
                   style: TextStyle(
                       fontSize: 25,
-                      color: Colors.white,
+                      color: Colors.black,
                       fontWeight: FontWeight.bold)),
               Text(dataModel.joiboniyantron,
-                  style: TextStyle(fontSize: 20, color: Colors.white)),
+                  style: TextStyle(fontSize: 20, color: Colors.black)),
               Divider(),
               Divider(),
               Text("রাসায়নিক নিয়ন্ত্রণ",
                   style: TextStyle(
                       fontSize: 25,
-                      color: Colors.white,
+                      color: Colors.black,
                       fontWeight: FontWeight.bold)),
               Text(dataModel.rasioniknyontron,
-                  style: TextStyle(fontSize: 20, color: Colors.white)),
+                  style: TextStyle(fontSize: 20, color: Colors.black)),
               Divider(),
               Divider(),
               Text("প্রতিরোধমূলক ব্যবস্থা",
                   style: TextStyle(
                       fontSize: 25,
-                      color: Colors.white,
+                      color: Colors.black,
                       fontWeight: FontWeight.bold)),
               Text(dataModel.bebostha,
-                  style: TextStyle(fontSize: 20, color: Colors.white)),
+                  style: TextStyle(fontSize: 20, color: Colors.black)),
               Divider(),
             ],
           ),
--- lib/home.dart
+++ lib/home.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tflite/tflite.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'details.dart';
 import 'model.dart';
@@ -119,114 +120,152 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.indigo,
+        backgroundColor: Colors.white,
         centerTitle: true,
+        actions: [
+          //png image
+          IconButton(
+            icon: Image.asset(
+              'assets/ju.png',
+              height: 30,
+              width: 30,
+            ),
+            onPressed: () {},
+          ),
+        ],
         title: Text(
           'পোকা মাকড় বিশারদ',
           style: TextStyle(
-            color: Colors.white,
+            color: Colors.green,
             fontWeight: FontWeight.w500,
             fontSize: 23,
           ),
         ),
       ),
-      body: Container(
-        color: Color.fromRGBO(68, 190, 255, 0.8),
-        padding: EdgeInsets.symmetric(horizontal: 35, vertical: 50),
+      body: SingleChildScrollView(
         child: Container(
-          alignment: Alignment.center,
-          padding: EdgeInsets.all(30),
-          decoration: BoxDecoration(
-            color: Colors.indigo,
-            borderRadius: BorderRadius.circular(30),
-          ),
+          padding: EdgeInsets.symmetric(horizontal: 35, vertical: 50),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                child: Center(
-                  child: _loading == true
-                      ? null //show nothing if no picture selected
-                      : Container(
-                          // child: Column(
-                          //   children: [
-                          //     Container(
-                          //       height: MediaQuery.of(context).size.width * 0.5,
-                          //       width: MediaQuery.of(context).size.width * 0.5,
-                          //       child: ClipRRect(
-                          //         borderRadius: BorderRadius.circular(30),
-                          //         child: Image.file(
-                          //           _image,
-                          //           fit: BoxFit.fill,
-                          //         ),
-                          //       ),
-                          //     ),
-                          //     Divider(
-                          //       height: 25,
-                          //       thickness: 1,
-                          //     ),
-                          //     // ignore: unnecessary_null_comparison
-                          //     _output != null
-                          //         ? Text(
-                          //             'The animal is: ${_output[0]['label']}',
-                          //             style: TextStyle(
-                          //               color: Colors.white,
-                          //               fontSize: 18,
-                          //               fontWeight: FontWeight.w400,
-                          //             ),
-                          //           )
-                          //         : Container(),
-                          //     Divider(
-                          //       height: 25,
-                          //       thickness: 1,
-                          //     ),
-                          //   ],
-                          // ),
-                          ),
-                ),
-              ),
-              Container(
-                child: Column(
-                  children: [
-                    GestureDetector(
-                      onTap: pickImage,
-                      child: Container(
-                        width: MediaQuery.of(context).size.width - 200,
-                        alignment: Alignment.center,
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 24, vertical: 17),
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                        child: Text(
-                          'ছবি তুলুন করুন',
-                          style: TextStyle(color: Colors.white, fontSize: 16),
+                child: Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(30),
+                  decoration: BoxDecoration(
+                    color: Color(0xffD8DBD2),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        child: Center(
+                          child: _loading == true
+                              ? null //show nothing if no picture selected
+                              : Container(),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 30),
-                    GestureDetector(
-                      onTap: pickGalleryImage,
-                      child: Container(
-                        width: MediaQuery.of(context).size.width - 200,
-                        alignment: Alignment.center,
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 24, vertical: 17),
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                        child: Text(
-                          'গ্যালারি থেকে ছবি নির্বাচন করুন',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(color: Colors.white, fontSize: 16),
+                      Container(
+                        child: Column(
+                          children: [
+                            Image.asset("assets/ss.png"),
+                            GestureDetector(
+                              onTap: pickImage,
+                              child: Container(
+                                // width: MediaQuery.of(context).size.width - 200,
+                                alignment: Alignment.center,
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 24, vertical: 17),
+                                decoration: BoxDecoration(
+                                  color: Colors.blue,
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                child: Text(
+                                  'ছবি তুলুন করুন',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 16),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 30),
+                            GestureDetector(
+                              onTap: pickGalleryImage,
+                              child: Container(
+                                // width: MediaQuery.of(context).size.width - 200,
+                                alignment: Alignment.center,
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 24, vertical: 17),
+                                decoration: BoxDecoration(
+                                  color: Colors.blue,
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                child: Text(
+                                  'গ্যালারি থেকে ছবি নির্বাচন করুন',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 16),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
+              SizedBox(height: 30),
+              InkWell(
+                onTap: () {
+                  //open url
+                  launch(
+                      'https://www.accuweather.com/en/bd/dhaka/28143/weather-forecast/28143');
+                },
+                child: Container(
+                  width: 600,
+                  padding: EdgeInsets.symmetric(horizontal: 35, vertical: 50),
+                  decoration: BoxDecoration(
+                    color: Color(0xffBE9EDF),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30),
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      Text('ঢাকা \n30 ডিগ্রি সেন্টিগ্রেড',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 20,
+                          )),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              InkWell(
+                onTap: () {
+                  //open url
+                  launch(
+                      'http://www.bmda.gov.bd/site/page/dbd79668-bd37-4e84-a396-b599bc0b040b/%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%B6%E0%A6%BF%E0%A6%95%E0%A7%8D%E0%A6%B7%E0%A6%A8-%E0%A6%93-%E0%A6%9A%E0%A6%BE%E0%A6%B7%E0%A6%BE%E0%A6%AC%E0%A6%BE%E0%A6%A6-%E0%A6%B8%E0%A6%82%E0%A6%95%E0%A7%8D%E0%A6%B0%E0%A6%BE%E0%A6%A8%E0%A7%8D%E0%A6%A4-%E0%A6%AE%E0%A7%8D%E0%A6%AF%E0%A6%BE%E0%A6%A8%E0%A7%81%E0%A7%9F%E0%A6%BE%E0%A6%B2');
+                },
+                child: Container(
+                  width: 600,
+                  padding: EdgeInsets.symmetric(horizontal: 35, vertical: 50),
+                  decoration: BoxDecoration(
+                    color: Color(0xffBE9EDF),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30),
+                    ),
+                  ),
+                  child: Text('চাষাবাদ সম্পর্কিত সকল তথ্য এখানে পাবেন',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                      )),
+                ),
+              )
             ],
           ),
         ),
